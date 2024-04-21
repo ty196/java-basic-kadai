312accc29dea3690b4a4a96947d1dd4fce7437d9
ty196 <162804784+ty196@users.noreply.github.com>
2024-04-21 20:48:15 +0900

    kadai_018

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,20 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+	public static void main(String[] args) {
+		KatoTaro_Chapter18 kato = new KatoTaro_Chapter18();
+		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+		
+		kato.setGivenName();
+		ichiro.setGivenName();
+		hanako.setGivenName();
+		
+		kato.execIntroduce();
+		ichiro.execIntroduce();
+		hanako.execIntroduce();
+				
+		
+	}
+
+}
---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,11 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+	public void setGivenName() {
+		this.givenName = "一郎";
+	}
+	void eachIntroduce() {
+		System.out.println("好きな食べ物はリンゴです");
+	}
+
+}
